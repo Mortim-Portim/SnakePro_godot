9839608746a00e29d10c2f7e8cc7f5aa9109ab4f
mortim-portim <kp>
2021-05-08 14:53:56 +0200

    comments for snake.gd

--- Scenes/Snake.gd
+++ Scenes/Snake.gd
@@ -1,48 +1,67 @@
 extends Node
 
+# Index of the character/hero and the index of the tiles to be used
 export var idx = 0
+# Start length of the snake in tiles
 export var startLength = 3.0
+# Start position of the snake
 export var startPos = Vector2.ZERO
+# Start direction of the snake
 export var startDir = Vector2.DOWN
 
+# Name of the Player
 var playerName = ""
+# Array of tiles belonging to the snakes body
 var tiles = [Vector2.ZERO]
+# Amount of tiles to be appended to the snake, each time the snake moves this is reduced by 1 but therefore the last snake tile is not removed, increasing the length of the snake 
 var fett = 0.0
+# Score to be set (e.g. the length or kills)
 var score = 1.0
+# Current direction of the Snake
 var currentDir = Vector2.DOWN
-var readyForNextTurn = true
+# Set by changeDir, is applied before the next movement
+var nextDir = currentDir
 
-# Called when the node enters the scene tree for the first time.
+# Resets the snake at the beginning
 func _ready():
 	reset()
 
+# Resets the snake based on startLength, startPos, startDir
 func reset():
 	fett = startLength-1
 	tiles = [startPos]
 	currentDir = startDir
 	score = 1.0
-	readyForNextTurn = true
+	nextDir = currentDir
 
+# Changes the direction of the snake before the next movement
+func changeDir(dir):
+	nextDir = dir
+# Changes the direction to the left of currentDir
 func to_left():
 	changeDir(currentDir.rotated(-PI/2.0).round())
+# Changes the direction to the right of currentDir
 func to_right():
 	changeDir(currentDir.rotated(PI/2.0).round())
-func changeDir(dir):
-	if readyForNextTurn:
-		currentDir = dir
-		readyForNextTurn = false
 
+# Applies nextDir and Moves the snake one tile in that direction
 func move():
-	readyForNextTurn = true
-	tiles.push_front(tiles.front()+currentDir)
+	# Set new direction
+	currentDir = nextDir
+	# Add the new head in front of the old head
+	tiles.push_front(get_head()+currentDir)
 	if fett > 0:
+		# Grows the snake by one tile, but reduces fett by one
 		fett -= 1
 	else:
+		# remove the last tile so that the length of the snake stays the same
 		tiles.pop_back()
 
+# Returns the tile representing the head of the snake
 func get_head():
 	return tiles.front()
 
+# Returns the direction from p1 to p2
 func dir_to_point(p1, p2):
 	var dif = p2-p1
 	if abs(dif.x) > abs(dif.y):
@@ -56,7 +75,7 @@ func dir_to_point(p1, p2):
 		else:
 			return Vector2.UP
 
-
+# Returns a dictionary containing all necassary information to completely recreate the snake
 func save():
 	var xVec = []
 	var yVec = []
@@ -78,7 +97,8 @@ func save():
 		"score" : 			score,
 		"currentDir.x" : 	currentDir.x,
 		"currentDir.y" : 	currentDir.y,
-		"readyForNextTurn" :readyForNextTurn,
+		"nextDir.x" :		nextDir.x,
+		"nextDir.y" :		nextDir.y,
 		"xVec" : 			xVec,
 		"yVec" : 			yVec,
 	}
